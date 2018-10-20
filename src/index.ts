870091f0e130618e1422f17ee272427c77a575e3
Stephen Haberman <stephen.haberman@gmail.com>
2018-10-20 17:58:06 -0500

    Add packaging.

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+
+export { ClassSpec } from './ClassSpec';
+export { CodeBlock } from './CodeBlock';
+export { DecoratorSpec } from './DecoratorSpec';
+export { EnumSpec } from './EnumSpec';
+export { FileSpec } from './FileSpec';
+export { FunctionSpec } from './FunctionSpec';
+export { InterfaceSpec } from './InterfaceSpec';
+export { Modifier } from './Modifier';
+export { ModuleSpec } from './ModuleSpec';
+export { ParameterSpec } from './ParameterSpec';
+export { PropertySpec } from './PropertySpec';
+export { SymbolSpecs } from './SymbolSpecs';
+export { TypeAliasSpec } from './TypeAliasSpec';
+export { TypeName, TypeNames } from './TypeNames';
