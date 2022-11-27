31f30dfb40d1fd1dccea1233b39d140a440d583f
Stephen Haberman <stephen.haberman@gmail.com>
2022-11-27 10:27:54 -0600

    chore: Remove usedConditionals global variable.

--- src/ConditionalOutput.ts
+++ src/ConditionalOutput.ts
@@ -8,7 +8,7 @@ import { Code } from "./Code";
  * site, i.e. declaring a `function someHelper() { ... }`, and calling it
  * like `someHelper()`.
  *
- * While generating code, you can make usage says by using `someHelper` as
+ * While generating code, you can make usage sites by using `someHelper` as
  * a placeholder, and then output the declaration with `someHelper.ifUsed`
  * to output the declaration conditionally only if `someHelper` has been
  * seen in the tree.
@@ -16,15 +16,20 @@ import { Code } from "./Code";
  * ```typescript
  * const someHelper = conditionalOutput(
  *   "someHelper",
- *   code`function someHelper() { return 1 } `
+ *   code`function someHelper(n: number) { return n * 2; } `
  * );
  *
  * const code = code`
- *   ${someHelper}
+ *   ${someHelper}(1);
  *
  *   ${someHelper.ifUsed}
  * `
  * ```
+ *
+ * In the above scenario, it's obvious that `someHelper` is being used, but in
+ * code generators with misc configuration options and conditional output paths
+ * (i.e. should I output a date helper if dates are even used for this file?)
+ * it is harder to tell when exactly a helper should/should not be included.
  */
 export class ConditionalOutput extends Node {
   // A given ConditionalOutput const could be used in multiple code
@@ -34,16 +39,17 @@ export class ConditionalOutput extends Node {
     super();
   }
 
+  /** Returns the declaration code, typically to be included near the bottom of your output as top-level scope. */
+  get ifUsed(): MaybeOutput {
+    return new MaybeOutput(this, this.declarationSiteCode);
+  }
+
   get childNodes(): unknown[] {
     return [this.declarationSiteCode];
   }
 
   toCodeString(): string {
     return this.usageSiteName;
-  }
-
-  get ifUsed(): MaybeOutput {
-    return new MaybeOutput(this, this.declarationSiteCode);
   }
 }
 
--- src/Node.ts
+++ src/Node.ts
@@ -1,6 +1,8 @@
+import { ConditionalOutput } from "./ConditionalOutput";
+
 export abstract class Node {
   /** Return the unformatted code for this node. */
-  abstract toCodeString(): string;
+  abstract toCodeString(used: ConditionalOutput[]): string;
 
   /** Any potentially string/SymbolSpec/Code nested nodes within us. */
   abstract get childNodes(): unknown[];
